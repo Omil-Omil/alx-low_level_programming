26d95385c902b9244eeb8c708efe2569be3df30d
Omil-Omil <omil.john1777@gmail.com>
2023-06-15 16:42:42 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * Main -Entry point
+ * Return: Always 0 (Succcess)
+ */
+int main(void0
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
