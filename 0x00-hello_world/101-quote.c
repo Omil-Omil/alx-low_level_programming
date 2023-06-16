abc1eb9453ec730a53fb08a513c7c2b9aae4175d
Omil-Omil <omil.john1777@gmail.com>
2023-06-16 09:40:37 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
 
 int main(void)
 {
-	write(1, "and that piece of art useful\n* - Dora Korpar, 2015-10-19\n", 59);
+	write(STDERR_FILENO, "and that piece of art useful\" - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
