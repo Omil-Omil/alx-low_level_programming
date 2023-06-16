53295631e6adc37ecbcbb6750139e239115072bc
Omil-Omil <omil.john1777@gmail.com>
2023-06-16 09:01:36 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 include <stdio.h>
 include <unistd.h>
 /**
- * mai - Entry point
+ * main - Entry point
  *
  * Return: Always 1 (Error)
  */
+
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(1, "and that piece of art useful\* - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
