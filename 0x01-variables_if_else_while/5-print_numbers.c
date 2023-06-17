ff8f5ffa291c7c1fdcb262e30499feddd46b7bab
Omil-Omil <omil.john1777@gmail.com>
2023-06-16 17:29:07 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints digit numbers of base ten starting from zero
+ *
+ * Return 0 (successful)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+
+	return (0);
+}
