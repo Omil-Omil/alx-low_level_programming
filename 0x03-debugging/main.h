21c8fa3aaa40342d3c4a2b93df9ae7e776ba5a29
Omil-Omil <omil.john1777@gmail.com>
2023-06-22 06:45:14 -0700

    main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,8 +1,9 @@
 #ifndef MAIN_H
-#define MAIN_H
+#define MAIN_
+H
 #include <stdio.h>
 
-void positive_or_negativ(int);
+void positive_or_negative(int);
 int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
