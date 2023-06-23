f2203afaa7d2fb06d2e9ff78480efe22a887ac42
Omil-Omil <omil.john1777@gmail.com>
2023-06-23 10:40:07 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * main - Test the function for positive or negative
+ * main - tests function is positive or negative
  * Return: 0
  */
 int main(void)
 {
 	int i;
+
 	i = 0;
-	
 	positive_or_negative(i);
 
-return (0);
+	return (o);
 }
