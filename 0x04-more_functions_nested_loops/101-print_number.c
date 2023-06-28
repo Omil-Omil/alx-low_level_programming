9afa945eb9dbea05e7f1e679c6fcf80851590c38
Omil-Omil <omil.john1777@gmail.com>
2023-06-28 15:51:06 -0700

    ./101-print_numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,18 +5,18 @@
  *
  * Return: Always 0
  */
-void print_numbers(int n)
+void print_number(int n)
 {
 	unsigned int i = n;
 
 	if (n < 0)
 	{
-		_putchar(4);
+		_putchar(45);
 		i = -i;
 	}
-	if (i / 100)
+	if (i / 10)
 	{
-		print_numbers(i / 10);
+		print_number(i / 10);
 	}
 	_putchar(i % 10 + '0');
 }
