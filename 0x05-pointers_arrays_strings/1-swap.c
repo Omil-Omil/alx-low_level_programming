b85f31ab421349046415b269375dfb9cf95e5f8d
Omil-Omil <omil.john1777@gmail.com>
2023-07-09 08:10:31 -0700

    update

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#inlude "main.h"
+/**
+ * swap_int - swaps the values of two integers
+ * @a: intager to swap
+ * @b: intager to swap
+ */
+void swap_int(int *a, int *b)
+{
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+}
