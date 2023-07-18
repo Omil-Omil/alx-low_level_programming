6fd5c3eadf04db5eb98a9df0c28bda01409af48b
Omil-Omil <omil.john1777@gmail.com>
2023-07-18 15:53:06 -0700

    5-free_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * init_dog - initialize variable of type struct dog
+ * @d: point to struct dog initialized
+ * @name: name that is to be initialized
+ * @age: the age to initialize
+ * @owner:owner to be initialized
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+{
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
